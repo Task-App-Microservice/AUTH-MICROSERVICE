51d5698a33b1e8341c7314179e35dcb3dfc89dda
ebraimsambo <ebraimsambo@gmail.com>
2025-03-26 02:21:36 +0100

    cors config

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,{
     cors: {
-      origin: "*"
+      origin: [
+        "https://tasl-app.onrender.com",
+        "http://localhost:3000"
+      ]
     }
   });
   app.useGlobalPipes(
