18ac6ab15887bf4703d638335e577113cecfb56b
ebraimsambo <ebraimsambo@gmail.com>
2025-03-26 01:57:03 +0100

    orgin all

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,{
-    cors: true
+    cors: {
+      origin: "*"
+    }
   });
   app.useGlobalPipes(
     new ValidationPipe({
